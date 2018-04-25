bbeca9a16d20049cb14dde5ca6d5aae73aea6a6a
Chris Nelson <perch24@gmail.com>
2018-04-24 19:09:56 -0500

    Added JWT auth.

--- services/users/project/api/models.py
+++ services/users/project/api/models.py
@@ -1,3 +1,6 @@
+import datetime
+
+import jwt
 from flask import current_app
 
 from project import db, bcrypt
@@ -15,6 +18,36 @@ class User(db.Model):
         self.username = username
         self.email = email
         self.password = bcrypt.generate_password_hash(password, current_app.config.get('BCRYPT_LOG_ROUNDS')).decode()
+
+    @staticmethod
+    def encode_auth_token(user_id):
+        """Generates the auth token"""
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(
+                    days=current_app.config.get('TOKEN_EXPIRATION_DAYS'),
+                    seconds=current_app.config.get('TOKEN_EXPIRATION_SECONDS')),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+            return jwt.encode(
+                payload,
+                current_app.config.get('SECRET_KEY'),
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        """Decode the auth token"""
+        try:
+            payload = jwt.decode(auth_token, current_app.config.get('SECRET_KEY'))
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please login again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please login again.'
 
     def to_json(self):
         return {
