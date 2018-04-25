5f6cfa45f69a30beea4f2e90eaf996fd7685c8e8
Chris Nelson <perch24@gmail.com>
2018-04-25 10:39:14 -0500

    Added registration and login/logout.

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -3,6 +3,7 @@ import {Route, Switch} from 'react-router-dom';
 import axios from 'axios';
 import About from './components/About';
 import Form from './components/Form';
+import Logout from './components/Logout';
 import NavBar from './components/NavBar';
 import UsersList from './components/UsersList';
 import AddUser from './components/AddUser';
@@ -17,10 +18,9 @@ export default class App extends Component {
                 username: '',
                 email: '',
                 password: ''
-            }
+            },
+            isAuthenticated: false
         };
-        this.addUser = this.addUser.bind(this);
-        this.handleChange = this.handleChange.bind(this);
     };
 
     componentDidMount() {
@@ -37,7 +37,7 @@ export default class App extends Component {
             });
     };
 
-    addUser(event) {
+    addUser = (event) => {
         event.preventDefault();
         const data = {
             username: this.state.username,
@@ -53,11 +53,50 @@ export default class App extends Component {
             });
     }
 
-    handleChange(event) {
+    handleChange = (event) => {
         const obj = {};
         obj[event.target.name] = event.target.value;
         this.setState(obj);
     };
+
+    handleFormChange = (event) => {
+        const obj = this.state.formData;
+        obj[event.target.name] = event.target.value;
+        this.setState(obj);
+    };
+
+    handleUserFormSubmit = (event) => {
+        event.preventDefault();
+        const formType = window.location.href.split('/').reverse()[0];
+        let data = {
+            email: this.state.formData.email,
+            password: this.state.formData.password,
+        };
+        if (formType === 'register') {
+            data.username = this.state.formData.username;
+        }
+        const url = `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/${formType}`
+        axios.post(url, data)
+            .then((res) => {
+                this.setState({
+                    formData: {username: '', email: '', password: ''},
+                    username: '',
+                    email: '',
+                    isAuthenticated: true
+                });
+                window.localStorage.setItem('authToken', res.data.auth_token);
+                this.getUsers();
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    logoutUser = () => {
+        window.localStorage.clear();
+        this.setState({isAuthenticated: false});
+    };
+
 
     render() {
         return (
@@ -89,12 +128,24 @@ export default class App extends Component {
                                     <Form
                                         formType={'Register'}
                                         formData={this.state.formData}
+                                        handleUserFormSubmit={this.handleUserFormSubmit}
+                                        handleFormChange={this.handleFormChange}
+                                        isAuthenticated={this.state.isAuthenticated}
                                     />
                                 )}/>
                                 <Route exact path='/login' render={() => (
                                     <Form
                                         formType={'Login'}
                                         formData={this.state.formData}
+                                        handleUserFormSubmit={this.handleUserFormSubmit}
+                                        handleFormChange={this.handleFormChange}
+                                        isAuthenticated={this.state.isAuthenticated}
+                                    />
+                                )}/>
+                                <Route exact path='/logout' render={() => (
+                                    <Logout
+                                        logoutUser={this.logoutUser}
+                                        isAuthenticated={this.state.isAuthenticated}
                                     />
                                 )}/>
                             </Switch>
@@ -102,7 +153,7 @@ export default class App extends Component {
                     </div>
                 </div>
             </div>
-    )
+        )
     };
 
-    };
+};
